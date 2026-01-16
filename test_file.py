399f56f2085a4a08ed3869ee32bf8287ae6ce049
Adityaptil51 <adityaapatil51@gmail.com>
2026-01-16 19:15:25 +0530

    saved

--- test_file.py
+++ test_file.py
@@ -1,9 +1,11 @@
-from test import product_details():
-expected_output = (
-    "product_name: Headphones\n"
-    "product_id: HP1001\n"
-    "quanity: 2\n"
-    "price: 199.99"
-)
+from test import product_details
+
+def test_product_details():
+    expected_output = (
+        "product_name: Headphones\n"
+        "product_id: HP1001\n"
+        "quanity: 2\n"
+        "price: 199.99"
+    )
 
 assert product_details("Headphones", "HP1001", 2, 199.99) == expected_output
