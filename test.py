d6a352540ef9091ad8cbee6a0159912f171ed1e3
Adityaptil51 <adityaapatil51@gmail.com>
2026-01-16 19:14:13 +0530

    saved

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
-def square(n):
-    result = n * n
-    print("Square is:", result)
-    return result
+def product_details(product_name, product_id,quanity,price):
+    return (
+        f"product_name: {product_name}\n"
+        f"product_id: {product_id}\n"
+        f"quanity: {quanity}\n"
+        f"price: {price}"
+    )
+
+if __name__ == "__main__":
+    product_name = "Laptop"
+    product_id = "LP1001"
+    quanity = 5
+    price = 999.99
+    print(product_details(product_name, product_id,quanity,price))
--- test_file.py
+++ test_file.py
@@ -1,3 +1,9 @@
-from test import square
-def test_square():
-    assert square(2) == 4
+from test import product_details():
+expected_output = (
+    "product_name: Headphones\n"
+    "product_id: HP1001\n"
+    "quanity: 2\n"
+    "price: 199.99"
+)
+
+assert product_details("Headphones", "HP1001", 2, 199.99) == expected_output
